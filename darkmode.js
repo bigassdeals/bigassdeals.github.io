951fd7cede7e67390f3f144976a3dd7b6c615fec
Luke Van Seters <lukevanseters@gmail.com>
2019-04-21 07:39:44 -0400

    Persist mode choice

--- darkmode.js
+++ darkmode.js
@@ -1,20 +1,47 @@
-function onDarkModeClick() {
-  const button = event.target;
-  const body = document.body;
+function loadStyleModeFromCookie() {
+  const mode = getCookieValue('mode');
+  if (mode != null) {
+    setStyleMode(mode);
+  }
+}
 
-  if (button.value === 'dark') {
+function onDarkModeClick(event) {
+  const button = event.target;
+  const mode = button.value;
+
+  setStyleMode(mode);
+}
+
+function setStyleMode(mode) {
+  const body = document.body;
+  const button = document.getElementById('dark-mode');
+
+  if (mode === 'dark') {
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+
     button.value = 'light'
     button.innerHTML = 'Light Mode'
     button.style.backgroundColor = '#333';
     button.style.color = 'white';
-    body.style.backgroundColor = 'black';
-    body.style.color = 'white';
-  } else {
+
+  } else if (mode === 'light') {
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+
     button.value = 'dark'
     button.innerHTML = 'Dark Mode'
     button.style.backgroundColor = '#FFF';
     button.style.color = 'black';
-    body.style.backgroundColor = 'white';
-    body.style.color = 'black';
   }
+
+  document.cookie = 'mode=' + mode
+}
+
+function getCookieValue(name) {
+    var values = document.cookie.match(
+      '(^|[^;]+)\\s*' + name + '\\s*=\\s*([^;]+)'
+    );
+
+    return values ? values.pop() : null;
 }
