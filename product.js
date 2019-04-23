c9c10472f4e48f67f14a224203cd214ef6227277
Luke Van Seters <lukevanseters@gmail.com>
2019-04-22 20:45:47 -0400

    Separate product info into a json file

---
+++ product.js
@@ -0,0 +1,29 @@
+function loadProduct() {
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      handleProductData(JSON.parse(xhttp.responseText));
+    }
+  };
+
+  xhttp.open('GET', 'product.json', true);
+  xhttp.send();
+}
+
+function handleProductData(product) {
+  const link = document.getElementById('deal-link');
+  const linkVisited = document.querySelector('#deal-link:visited');
+  const image = document.getElementById('deal-pic');
+
+  link.innerHTML = product.title;
+  link.href = product.url;
+
+  link.style.color = product.color;
+
+  if (linkVisited) {
+    linkVisited.style.color = product.color;
+  }
+
+  image.src = product.image_url;
+}
